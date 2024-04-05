717b856dbf14eb0ac5529396a4ac01590b316327
andre_abreu <andre_abreu@sicredi.com.br>
2024-04-05 14:00:05 -0300

    add principal file.java

---
+++ src/main/java/com/mycompany/sales_system/Sales_system.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.sales_system;
+
+import com.mycompany.sales_system.view.Principal;
+
+
+public class Sales_system {
+    
+    public static void main(String[] args) throws Exception {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    } 
+    
+}
+
